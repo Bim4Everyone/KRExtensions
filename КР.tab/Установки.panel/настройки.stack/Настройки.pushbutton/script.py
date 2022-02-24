d7f197deca01d010110ebb65333f5f7a08f74172
dosymep <biseuv@yandex.ru>
2022-02-24 15:34:46 +0300

    init

---
+++ КР.tab/Установки.panel/настройки.stack/Настройки.pushbutton/script.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from pyrevit.loader.sessionmgr import execute_command
+execute_command("01dotbim-bim-установки-настройки-настройки")
+
---
+++ КР.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from pyrevit.loader.sessionmgr import execute_command
+execute_command("01dotbim-bim-установки-настройки-обновить")
