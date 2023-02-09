ad38f6a654fe1c24f3b468bf0a88d3991a3f7f60
sssenya <senia7@bk.ru>
2023-02-09 21:02:50 +0300

    Обновлен скрипт КР для создания таблиц УПК.

--- КР.tab/Арматура.panel/Таблицы УПК.pushbutton/script.py
+++ КР.tab/Арматура.panel/Таблицы УПК.pushbutton/script.py
@@ -6,6 +6,9 @@ from System.Collections.Generic import *
 clr.AddReference("dosymep.Revit.dll")
 clr.AddReference("dosymep.Bim4Everyone.dll")
 
+from System.Windows.Input import ICommand
+
+import pyevent
 from pyrevit import EXEC_PARAMS, revit
 from pyrevit.forms import *
 from pyrevit import script
@@ -33,50 +36,70 @@ class RevitRepository:
         self.doc = doc
         self.categories = table_type.categories
         self.type_key_word = table_type.type_key_word
+        self.quality_indexes = table_type.quality_indexes
 
         self.__concrete = self.__get_concrete()
         self.__buildings = self.__get_buildings()
         self.__construction_sections = self.__get_construction_sections()
-        # self.__rebar = self.__get_rebar()
+        self.__rebar = self.__get_rebar()
+
+    def update_elements_info(self):
+        pass
+
+    def get_filtered_concrete(self, buildings, constr_sections):
+        buildings = [x.text_value for x in buildings if x.is_checked]
+        constr_sections = [x.text_value for x in constr_sections if x.is_checked]
+        filtered_elements = []
+        for element in self.concrete:
+            param1 = element.LookupParameter("ФОП_Секция СМР")
+            param2 = element.LookupParameter("обр_ФОП_Раздел проекта")
+
+            if self.get_param_value(param1) in buildings:
+                if self.get_param_value(param2) in constr_sections:
+                    filtered_elements.append(element)
+
+        return filtered_elements
+
+    def get_filtered_rebar(self, buildings, constr_sections):
+        buildings = [x.text_value for x in buildings if x.is_checked]
+        constr_sections = [x.text_value for x in constr_sections if x.is_checked]
+        filtered_elements = []
+        for element in self.rebar:
+            param1 = element.LookupParameter("ФОП_Секция СМР")
+            param2 = element.LookupParameter("обр_ФОП_Раздел проекта")
+
+            if self.get_param_value(param1) in buildings:
+                if self.get_param_value(param2) in constr_sections:
+                    filtered_elements.append(element)
+
+        return filtered_elements
 
     def __create_param_set(self, elements, param_name):
         set_of_values = set()
         for element in elements:
             param = element.LookupParameter(param_name)
-            if param.HasValue:
-                set_of_values.add(param.AsString())
-            else:
-                set_of_values.add("<Параметр не заполнен>")
+            set_of_values.add(self.get_param_value(param))
         return sorted(set_of_values)
 
     def __get_buildings(self):
         buildings = self.__create_param_set(self.concrete, "ФОП_Секция СМР")
         result_buildings = []
         for building in buildings:
-            result_buildings.append(Building(building))
+            result_buildings.append(ElementSection(building))
         return result_buildings
 
     def __get_construction_sections(self):
         sections = self.__create_param_set(self.concrete, "обр_ФОП_Раздел проекта")
         result_sections = []
         for section in sections:
-            result_sections.append(ConstructionSection(section))
+            result_sections.append(ElementSection(section))
         return result_sections
-
-    def __create_param_filter(self, name):
-        fvp = ParameterValueProvider(ElementId(BuiltInParameter.VIEW_NAME))
-        rule = FilterStringEquals()
-        value = name
-        case_sens = False
-        filter_rule = FilterStringRule(fvp, rule, value, case_sens)
-        name_filter = ElementParameterFilter(filter_rule)
-        return name_filter
 
     def __filter_by_param(self, elements, param_name, value):
         filtered_list = []
         for element in elements:
-            param_value = str(element.LookupParameter(param_name).AsString())
-            if param_value != value:
+            param = element.LookupParameter(param_name)
+            if self.get_param_value(param) != value:
                 filtered_list.append(element)
         return filtered_list
 
@@ -102,11 +125,23 @@ class RevitRepository:
         return elements
 
     def __get_rebar(self):
-        elements = FilteredElementCollector(self.doc).OfCategory(BuiltInCategory.OST_Rebar).ToELements()
+        elements = FilteredElementCollector(self.doc).OfCategory(BuiltInCategory.OST_Rebar)
+        elements.WhereElementIsNotElementType().ToElements()
         elements = self.__filter_by_param(elements, "обр_ФОП_Фильтрация 1", "Исключить из показателей качества")
         elements = self.__filter_by_param(elements, "обр_ФОП_Фильтрация 2", "Исключить из показателей качества")
+        elements_new = []
+        for value in self.quality_indexes.values():
+            elements_new += self.__filter_by_param(elements, "обр_ФОП_Группа КР", value)
+
         # elements = self.__filter_by_param(elements, "обр_ФОП_Форма номер", "1000")
-        return elements
+        return elements_new
+
+    @staticmethod
+    def get_param_value(param):
+        if param.HasValue:
+            return param.AsString()
+        else:
+            return ""
 
     @reactive
     def concrete(self):
@@ -130,6 +165,7 @@ class TableType:
         self.__name = name
         self.__type_key_word = ""
         self.__categories = []
+        self.__quality_indexes = dict()
 
     @reactive
     def name(self):
@@ -155,22 +191,22 @@ class TableType:
     def categories(self, value):
         self.__categories = value
 
+    @reactive
+    def quality_indexes(self):
+        return self.__quality_indexes
 
-class Construction:
-    """
-    Класс для расчетов всех показателей.
-    """
-
-    def __init__(self):
-        pass
-
-    def get_concrete_volume(self):
-        pass
+    @quality_indexes.setter
+    def quality_indexes(self, value):
+        self.__quality_indexes = value
 
 
-class Building:
-    def __init__(self, number):
-        self.__number = number
+class ElementSection:
+    def __init__(self, text_value):
+        self.__text_value = text_value
+        if text_value:
+            self.__number = text_value
+        else:
+            self.__number = "<Параметр не заполнен>"
         self.__is_checked = False
 
     @reactive
@@ -182,26 +218,12 @@ class Building:
         self.__number = value
 
     @reactive
-    def is_checked(self):
-        return self.__is_checked
+    def text_value(self):
+        return self.__text_value
 
-    @is_checked.setter
-    def is_checked(self, value):
-        self.__is_checked = value
-
-
-class ConstructionSection:
-    def __init__(self, section_name):
-        self.__section_name = section_name
-        self.__is_checked = False
-
-    @reactive
-    def section_name(self):
-        return self.__section_name
-
-    @section_name.setter
-    def section_name(self, value):
-        self.__section_name = value
+    @text_value.setter
+    def text_value(self, value):
+        self.__text_value = value
 
     @reactive
     def is_checked(self):
@@ -210,18 +232,103 @@ class ConstructionSection:
     @is_checked.setter
     def is_checked(self, value):
         self.__is_checked = value
+
+
+class Construction:
+    """
+    Класс для расчетов всех показателей.
+    """
+
+    def __init__(self, concrete_elements, rebar_elements):
+        self.__concrete = concrete_elements
+        self.__rebar = rebar_elements
+
+        self.rebar_by_function = dict()
+        self.rebar_mass_by_function = dict()
+
+        self.group_rebar_by_function()
+        self.calculate_quality_indexes()
+        self.calculate_concrete_volume()
+
+    def calculate_concrete_volume(self):
+        volume = 0
+        for element in self.__concrete:
+            volume += element.LookupParameter("Объем").AsDouble()
+
+    def calculate_rebar_mass(self, elements):
+        return 100
+
+    def group_rebar_by_function(self):
+        for element in self.__rebar:
+            rebar_function = element.LookupParameter("обр_ФОП_Группа КР").AsString()
+            self.rebar_by_function.setdefault(rebar_function, [])
+            self.rebar_by_function[rebar_function].append(element)
+
+    def calculate_quality_indexes(self):
+        for key in self.rebar_by_function.keys():
+            elements = self.rebar_by_function[key]
+            rebar_mass = self.calculate_rebar_mass(elements)
+            self.rebar_mass_by_function[key] = rebar_mass
+
+    def get_quality_index(self, name):
+        return self.rebar_mass_by_function[name]
 
 
 class QualityTable:
     """
     Класс для формирования спецификации.
     """
-
-    def __init__(self):
-        pass
+    def __init__(self, table_type, construction):
+        self.indexes = table_type.quality_indexes
+        self.construction = construction
 
     def create_table(self):
-        pass
+        output = script.get_output()
+        data = self.set_row_values()
+        output.print_table(table_data=data,
+                           title="Показатели качества",
+                           columns=["Название", "Значение"])
+
+    def set_row_values(self):
+        rows = []
+        for index_name in self.indexes.keys():
+            # alert(index_name)
+            row = []
+            row.append(index_name)
+            index = self.indexes[index_name]
+            value = self.construction.get_quality_index(index)
+            # alert(str(value))
+            row.append(str(value))
+            rows.append(row)
+        return rows
+
+
+class CreateQualityTableCommand(ICommand):
+    CanExecuteChanged, _canExecuteChanged = pyevent.make_event()
+
+    def __init__(self, view_model, *args):
+        ICommand.__init__(self, *args)
+        self.__view_model = view_model
+        self.__view_model.PropertyChanged += self.ViewModel_PropertyChanged
+
+    def add_CanExecuteChanged(self, value):
+        self.CanExecuteChanged += value
+
+    def remove_CanExecuteChanged(self, value):
+        self.CanExecuteChanged -= value
+
+    def OnCanExecuteChanged(self):
+        self._canExecuteChanged(self, System.EventArgs.Empty)
+
+    def ViewModel_PropertyChanged(self, sender, e):
+        self.OnCanExecuteChanged()
+
+    def CanExecute(self, parameter):
+        return True
+
+    def Execute(self, parameter):
+        self.__view_model.quality_table.create_table()
+        # alert(str(len(self.__view_model.quality_table)))
 
 
 class MainWindow(WPFWindow):
@@ -242,7 +349,9 @@ class MainWindowViewModel(Reactive):
         self.__revit_repository = None
         self.__buildings = []
         self.__construction_sections = []
-        self.__table_concrete = []
+        self.__quality_table = []
+
+        self.__create_tables_command = CreateQualityTableCommand(self)
 
     @reactive
     def table_types(self):
@@ -255,7 +364,6 @@ class MainWindowViewModel(Reactive):
     @selected_table_type.setter
     def selected_table_type(self, value):
         self.__revit_repository = RevitRepository(doc, value)
-        self.table_concrete = self.__revit_repository.concrete
         self.buildings = self.__revit_repository.buildings
         self.construction_sections = self.__revit_repository.construction_sections
         self.__selected_table_type = value
@@ -277,12 +385,20 @@ class MainWindowViewModel(Reactive):
         self.__construction_sections = value
 
     @reactive
-    def table_concrete(self):
-        return self.__table_concrete
+    def quality_table(self):
+        concrete = self.__revit_repository.get_filtered_concrete(self.buildings, self.construction_sections)
+        rebar = self.__revit_repository.get_filtered_rebar(self.buildings, self.construction_sections)
+        construction = Construction(concrete, rebar)
 
-    @table_concrete.setter
-    def table_concrete(self, value):
-        self.__table_concrete = value
+        return QualityTable(self.selected_table_type, construction)
+
+    # @quality_table.setter
+    # def quality_table(self, value):
+    #     self.__quality_table = value
+
+    @property
+    def create_tables_command(self):
+        return self.__create_tables_command
 
 
 @notification()
@@ -296,10 +412,15 @@ def script_execute(plugin_logger):
     walls_table_type = TableType("Стены")
     walls_table_type.categories = [walls_cat]
     walls_table_type.type_key_word = "Стена"
+    walls_table_type.quality_indexes = {"Масса вертикальной арматуры, кг": "Продольная арматура",
+                                  "Масса горизонтальной арматуры, кг": "Горизонтальная арматура",
+                                  "Масса конструктивной арматуры, кг": "Конструктивная арматура"}
 
     columns_table_type = TableType("Пилоны")
     columns_table_type.categories = [walls_cat, columns_cat]
     columns_table_type.type_key_word = "Пилон"
+    columns_table_type.quality_indexes = {"Масса продольной арматуры, кг": "Пилоны_Продольная",
+                                "Масса поперечной арматуры, кг": "Пилоны_Поперечная"}
 
     table_types.append(walls_table_type)
     table_types.append(columns_table_type)
