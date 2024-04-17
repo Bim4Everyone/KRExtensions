f274ad767febd4ac2918dfa8f43596e9a30347f2
Vladimir Stroganov <vlastroganov@gmail.com>
2024-04-17 09:18:41 +0300

    Настройки: Сделано обновление сессии без обновления репозиториев (#3)

--- КР.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
+++ КР.tab/Установки.panel/настройки.stack/Обновить.pushbutton/script.py
@@ -1,4 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from pyrevit.loader.sessionmgr import execute_command
-execute_command("01dotbim-bim-установки-настройки-обновить")
+import os
+import pyrevit.coreutils.git as libgit
+from pyrevit.versionmgr import updater
+from pyrevit import EXEC_PARAMS
+from pyrevit import forms
+
+
+from dosymep_libs.bim4everyone import *
+
+__cleanengine__ = True
+
+
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    if EXEC_PARAMS.executed_from_ui:
+        forms.alert('Вы уверены что хотите обновить?',
+                          yes=True, no=True, exitscript=True)
+
+    # пытаемся обновится
+    path = os.path.abspath(__file__)
+    repo_path = libgit.libgit.Repository.Discover(path)
+    repo_info = libgit.get_repo(repo_path)
+    updater.update_repo(repo_info)
+
+
+script_execute()
