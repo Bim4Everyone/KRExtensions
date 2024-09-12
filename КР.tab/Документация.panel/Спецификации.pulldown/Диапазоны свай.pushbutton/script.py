08cc12c864c18a9c11a9a4bf90752b344be01138
Дивин Никита Владимирович <Divin_n@a101.ru>
2024-09-12 18:11:58 +0300

    Часть функционала функций перенесено в класс RevitPileType

--- КР.tab/Документация.panel/Спецификации.pulldown/Диапазоны свай.pushbutton/script.py
+++ КР.tab/Документация.panel/Спецификации.pulldown/Диапазоны свай.pushbutton/script.py
@@ -30,11 +30,34 @@ app = doc.Application
 param_name_for_mark = "Марка"
 param_name_for_write = "ФОП_Примечание"
 
+# Список, который будет содержать id свай,у которых нет или не корректная марка
+pile_ids_without_mark = []
+
 class RevitPileType:
-    def __init__(self):
+    def __init__(self, pile_type_name):
+        self.pile_type_name = pile_type_name
         self.piles = []
         self.marks = []
+        self.all_marks = ""
         self.mark_range = ""
+
+    def add_pile(self, pile):
+        self.piles.append(pile)
+        mark_as_str = pile.GetParam(BuiltInParameter.ALL_MODEL_MARK).AsString()
+        if not mark_as_str:
+            pile_ids_without_mark.append(pile.Id)
+            return
+        # Пытаемся перевести марку сваи в число
+        try:
+            mark = int(mark_as_str)
+        except:
+            pile_ids_without_mark.append(pile.Id)
+            return
+        self.add_mark(mark)
+
+    def add_mark(self, mark):
+        self.marks.append(mark)
+        self.all_marks += '{}, '.format(mark)
 
     def get_range(self):
         self.marks.sort()
@@ -45,7 +68,7 @@ class RevitPileType:
                 self.mark_range += str(value_0) + ", "
             else:
                 self.mark_range += str(value_0) + "-" + str(value_1) + ", "
-        self.range = self.mark_range[:-2]
+        self.mark_range = self.mark_range[:-2]
 
     def get_ranges(self, list_of_marks):
         for a, b in itertools.groupby(enumerate(list_of_marks), lambda pair: pair[1] - pair[0]):
@@ -53,8 +76,10 @@ class RevitPileType:
             yield b[0][1], b[-1][1]
 
     def write_ranges(self):
+        if not self.piles:
+            print('При работе с типом {} не было отобрано ни одной сваи!'.format(self.pile_type_name))
         for pile in self.piles:
-            pile.GetParam(param_name_for_write).Set(self.range)
+            pile.GetParam(param_name_for_write).Set(self.mark_range)
 
 
 def get_piles():
@@ -71,30 +96,36 @@ def get_piles():
         alert("Выберите сваи", exitscript=True)
     return piles
 
-def get_piles_by_types(piles):
+
+def get_pile_types(piles):
     dictionary = {}
-    pile_ids_without_mark = []
     for pile in piles:
         pile_name = pile.Name
         if pile_name not in dictionary.keys():
-            dictionary[pile_name] = RevitPileType()
-        dictionary[pile_name].piles.append(pile)
+            dictionary[pile_name] = RevitPileType(pile_name)
+        dictionary[pile_name].add_pile(pile)
 
-        mark_as_str = pile.GetParam(param_name_for_mark).AsString()
-        if not mark_as_str:
-            pile_ids_without_mark.append(pile.Id)
-            continue
-        mark = int(mark_as_str)
-        dictionary[pile_name].marks.append(mark)
-    return dictionary, pile_ids_without_mark
+    return dictionary.values()
 
-def write_values_of_pile_ranges(dictionary):
+
+def write_values_of_pile_ranges(pile_types):
+    report = []
     with revit.Transaction("BIM: Диапазоны свай"):
-        for key in dictionary.keys():
-            revit_piles = dictionary[key]
-            revit_piles.get_range()
-            revit_piles.write_ranges()
-
+        for pile_type in pile_types:
+            pile_type.get_range()
+            pile_type.write_ranges()
+            # Формируем отчет по колонкам:
+            #   - имя типоразмера свай;
+            #   - марки свай этого типа через запятую;
+            #   - марки свай этого типа диапазоном;
+            report.append(
+                [
+                    pile_type.pile_type_name,
+                    pile_type.all_marks,
+                    pile_type.mark_range
+                ]
+            )
+    return report
 
 @notification()
 @log_plugin(EXEC_PARAMS.command_name)
@@ -106,16 +137,21 @@ def script_execute(plugin_logger):
     piles = get_piles()
     print("Собрано свай: " + str(len(piles)))
 
-    # В словаре элементы помещены в класс-оболочку RevitPileRange и сгруппированы по типам
-    dictionary, pile_ids_without_mark = get_piles_by_types(piles)
-    print("Из них разных типов: " + str(len(dictionary.keys())))
+    # Список типоразмеров свай, обернутых в класс-оболочку RevitPileType
+    pile_types = get_pile_types(piles)
+    print("Из них разных типов: " + str(len(pile_types)))
 
     # Выполняем расчет и запись диапазонов свай в экземпляры свай
-    write_values_of_pile_ranges(dictionary)
+    report = write_values_of_pile_ranges(pile_types)
 
+    print("В таблице ниже обработанные сваи:")
     output = script.output.get_output()
+    output.print_table(table_data=report,
+                       title="Отчет работы плагина",
+                       columns=["Тип сваи", "Марки свай", "Диапазон марок"])
+
     if pile_ids_without_mark:
-        print("Найдены не замаркированные сваи:")
+        print("\nНайдены не замаркированные сваи:")
         for pile_id_without_mark in pile_ids_without_mark:
             print('{}: {}'.format("- свая с id: ", output.linkify(pile_id_without_mark)))
 
