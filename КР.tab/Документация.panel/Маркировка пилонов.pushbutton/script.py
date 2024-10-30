dbb4710f6a09bee093e59113d439f16ca0501f84
Никита Дивин <nikitad93@mail.ru>
2024-10-30 11:15:11 +0300

    Добавлена кнопка для плагина Маркировка пилонов

---
+++ КР.tab/Документация.panel/Маркировка пилонов.pushbutton/script.py
@@ -0,0 +1,38 @@
+import clr
+import datetime
+
+from System.Collections.Generic import *
+
+clr.AddReference("dosymep.Revit.dll")
+clr.AddReference("dosymep.Bim4Everyone.dll")
+
+from System.Windows.Input import ICommand
+
+import pyevent
+from pyrevit import EXEC_PARAMS, revit
+from pyrevit.forms import *
+from pyrevit import script
+
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.UI.Selection import *
+
+import dosymep
+
+clr.ImportExtensions(dosymep.Revit)
+clr.ImportExtensions(dosymep.Bim4Everyone)
+
+from dosymep_libs.bim4everyone import *
+
+doc = __revit__.ActiveUIDocument.Document
+uidoc = __revit__.ActiveUIDocument
+app = doc.Application
+
+
+@notification()
+@log_plugin(EXEC_PARAMS.command_name)
+def script_execute(plugin_logger):
+    a = 0
+
+
+
+script_execute()
